c622c48e2370aa94727f8bdaa136eaa6a1509b1d
t==Bz&\Mh+r=:ChfA0;! <ziadelwali@gmail.com>
2026-09-23 21:27:13 +0200

    Change passphrase

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* Offline cache for Lift. App shell is precached; exercise photos are cached
    on first view. Stamped with a new VERSION on every publish. */
-const VERSION = 'v-1b0ecc8dcb';
+const VERSION = 'v-433ce9dac0';
 const SHELL = ['./', './index.html', './exercises.js', './engine.js', './fbconfig.js', './manifest.webmanifest', './icon-192.png', './icon-180.png'];
 
 self.addEventListener('install', function (e) {
